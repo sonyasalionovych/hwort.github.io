46d1ba2e26270bf38f749e36b5ad80e7ca5804ab
sonyasalionovych <33807874+sonyasalionovych@users.noreply.github.com>
2020-02-05 16:20:48 +0100

    Add files via upload

---
+++ dz24/dz24.js
@@ -0,0 +1,5 @@
+console.log(/[a-z0-9]*@(gmail|mail)\.(com|ru)$/.test("vasiapupkin@gmail.com"));
+console.log(/[a-z0-9]*@(gmail|mail)\.(com|ru)$/.test("inokentiiivanov@gmail.com"));
+console.log(/[a-z0-9]*@(gmail|mail)\.(com|ru)$/.test("boriapetrov@mail.ru"));
+//true
+//true
